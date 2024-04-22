d7df35e6572cc00b48a66a20a27d148da79ff466
AuroraVeil <Alexpeiser423@gmail.com>
2024-04-22 17:41:23 -0500

    make projectile

---
+++ Node3D.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") / 100
+@export var speed = 0.5
+var direction = Vector3.ZERO
+var velocityY = 0
+var dmg = 1
+func _ready():
+	set_as_top_level(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocityY -= gravity * delta 
+	position += direction * speed * delta * 60
+	position.y += velocityY 
+	
+
+
+func _on_area_3d_body_entered(body):
+	print(get_parent())
+	print(body)
+	if body.has_method("damage"):
+		body.damage(dmg)
--- movement.gd
+++ movement.gd
@@ -6,14 +6,16 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+var projectile = preload("res://projectile.tscn")
 @onready var cameraf = $fpv
 @onready var camera3 = $PivotPoint/tpv
 @onready var pivot = $PivotPoint
+@onready var rayRizzo = $RayCast3D
 func _process(delta):
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 func _physics_process(delta):
 	# Add the gravity.
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -22,10 +24,8 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_pressed("change_camera"):
 		if cameraf.is_current():
-			print("a")
 			camera3.make_current()
 		else:
-			print("b")
 			cameraf.make_current()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,6 +40,8 @@ func _physics_process(delta):
 	var turn_dir = (int)(Input.is_action_pressed("turn_left")) - (int)(Input.is_action_pressed("turn_right"))
 	self.rotate_y(turn_dir * 0.1)
 	move_and_slide()
+	if Input.is_action_just_pressed("shoot"):
+		attack(projectile)
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x / 100
@@ -48,3 +50,10 @@ func _input(event):
 		else:
 			pivot.rotation.x -= event.relative.y / 250
 			pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-45), deg_to_rad(45))
+func attack(projectile: PackedScene) -> void:
+	var atk = projectile.instantiate()
+	atk.position = global_position
+	atk.direction = -cameraf.get_global_transform().basis.z
+	get_parent().add_child(atk)
+func damage(dmg):
+	print("ouch")
