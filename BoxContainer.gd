e369f9fd4780d08ca8f5d09527bb2fd0a2caf3a6
AuroraVeil <Alexpeiser423@gmail.com>
2024-06-18 15:27:51 -0500

    Improve UI

--- BoxContainer.gd
+++ BoxContainer.gd
@@ -8,5 +8,12 @@ func _process(delta):
 	if handC:
 		for i in handC:
 			i.set_texture(null)
+			i.get_child(0).set_frame(15)
 		for i in range(0, PC.hand.size()):
+			
+			if PC.activeCard == i:
+				handC[i].position.y = 304
+			else:
+				handC[i].position.y = 384
 			handC[i].set_texture(PC.hand[i].sprite)
+			handC[i].get_child(0).set_frame(PC.hand[i].uses - 1)
--- CardPickup.gd
+++ CardPickup.gd
@@ -1,9 +1,13 @@
 extends Node3D
 
+@export var BobCurve: Curve
 @export var heldcard = load("res://dash_card.tscn")
+var midHeight
+var Cycle100 = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var temp = heldcard.instantiate()
+	midHeight = position.y
 	$Sprite3D.set_texture(temp.sprite)
 	$Sprite3D2.set_texture(temp.sprite)
 
@@ -11,3 +15,10 @@ func _on_area_3d_body_shape_entered(body_rid, body, body_shape_index, local_shap
 	if (body.has_method("addCard")):
 		body.addCard(heldcard)
 		queue_free()
+func _process(delta):
+	rotation.y += 1 * delta
+	Cycle100 += 1.0
+	if Cycle100 > 100:
+		Cycle100 = 0.0
+	if BobCurve:
+		position.y = midHeight + (BobCurve.sample(Cycle100 / 100) * .15)
